113533a1d3cae0243af8577176880af6c752ae55
Tamsin Huggins <tamsin.hns@gmail.com>
2023-09-22 22:59:29 +0100

    added photo. made the fonts nice. Tweaked the colors. Fixed the title header.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ function App() {
     <div className="App">
       <div className="A4Page">
         <Banner />
+
         <div className="eventContainer">
+          <div id="titleStrip">
+            <h1>TAMSIN HUGGINS</h1>
+            <p>
+              Here I will type my bio Components are independent and reusable
+              bits of code. They serve the same purpose as JavaScript functions,
+              but work in isolation and return HTML. Components come in two
+              types, Class components and Function components, in this tutorial
+              we will concentrate on Function components.
+            </p>
+          </div>
+
           <CvEvent />
           <CvEvent />
         </div>
